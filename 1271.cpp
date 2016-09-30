9052ffea19a5deecf201d60968be57424dde232a
Gabriel S. Pires <gabrielspires@live.com>
2016-09-30 19:24:02 -0300

    .

---
+++ 1271.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	int k, temp, consulta, numGenoma = 0;
+	int n,   	//num de genes [1:50000]
+		r,   	//num de inversoes
+		q,   	//num de consultas
+		i,j,	//indices das inversoes
+		genoma[50000]; //genoma
+
+	while(cin >> n >> r, n != 0){
+		numGenoma++;
+
+		for(i=0; i<n; i++) genoma[i] = i+1;
+		while(r--){
+			cin  >> i >> j;
+
+			for(k=i; k<=j; k++, j--){
+				temp = genoma[k-1];
+				genoma[k-1] = genoma[j-1];
+				genoma[j-1] = temp;
+			}
+		}
+
+		cin >> q;
+		cout << "Genome " << numGenoma << endl;
+
+		while(q--){
+			cin >> consulta;
+			for(i=0; i<n; i++){
+				if(genoma[i] == consulta){
+					cout << i+1 << endl;
+				}
+			}
+		}
+	}
+
+
+	return 0;
+}
